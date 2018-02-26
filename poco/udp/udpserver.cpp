359b81090fd1f6450ea1b4d7b325bf2afe6c3ef0
hexu <hexu_1985@sina.com>
2018-02-26 15:21:50 +0800

    add udpechoserver demo of poco

---
+++ poco/udp/udpserver.cpp
@@ -0,0 +1,21 @@
+#include "Poco/Net/DatagramSocket.h"
+#include "Poco/Net/IPAddress.h"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    Poco::Net::SocketAddress socketAddress(Poco::Net::IPAddress(), 1234);
+    Poco::Net::DatagramSocket datagramSocket(socketAddress);
+
+    char buffer[1024];
+
+    while (true)
+    {
+        Poco::Net::SocketAddress sender;
+        int n = datagramSocket.receiveFrom(buffer, sizeof(buffer) - 1, sender);
+        buffer[n] = '\0';
+        std::cout << sender.toString() << ": " << buffer << std::endl;
+    }
+
+    return 0;
+}
