3afc4b59cd8350b444d5ef49067b3c1e6a74c42f
hexu <hexu_1985@sina.com>
2018-05-07 11:37:53 +0800

    add apue3e fileio demo

--- lib/apue.h
+++ lib/apue.h
@@ -9,4 +9,14 @@
 
 #include "error.h"
 
+/*
+ * Default file access permissions for new files.
+ */
+#define	FILE_MODE	(S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+
+/*
+ * Default permissions for new directories.
+ */
+#define	DIR_MODE	(FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)
+
 #endif
